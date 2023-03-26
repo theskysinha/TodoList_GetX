cd1a55b6a5665e6ae7dd374ec6a89405325e04ce
Akash Sinha <akashsinha432003@gmail.com>
2023-03-26 16:45:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'app/modules/home/view.dart';
+  
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Todo List',
+      home: HomePage(),
+    );
+  }
+}
+
